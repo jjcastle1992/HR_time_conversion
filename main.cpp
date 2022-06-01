5e857c2ad230b872ca802fde3d9be4fbac065481
James <jjcastle@utexas.edu>
2022-05-31 21:05:41 -0500

    Added first pass at timeConversion function. Updated logic for hours transformations, forced uppercase on AM/PM and added section to convert hour transformed hours back into a string and pushing them into a militaryTime string that gets returned to the caller.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-
+#include <string>
 using namespace std;
 
 /*
@@ -10,17 +10,47 @@ using namespace std;
  */
 
 string timeConversion(string s) {
-//Capture first two characters of string as hh
+    string strHours = "";
+    string meridiem = "";
+    int hours = 0;
+    string militaryTime = "";
 
-//capture last to characters of string as meridiem
+    //Capture first two characters of string as hh
+    for (int i = 0; i < 2; i++) {
+        strHours += s[i];
+    }
+    //capture last to characters of string as meridiem
+    for (int i = 8; i < 10; i++) {
+        meridiem += s[i];
+    }
+    //convert strHours to int
+    hours = stoi(strHours);
+    //force meridiem to be upper case
+    transform(meridiem.begin(), meridiem.end(), meridiem.begin(), ::toupper);
 
-//convert hh to int
+    //if AM is last two characters and hh != 12 do nothing. If hh == 12, then subtract 12 from hh
+    if (meridiem == "AM" && hours == 12) {
+        hours -= 12;
+    }
 
-//if AM is last two characters and hh != 12 do nothing. If hh == 12, then subtract 12 from hh
+    //if PM is last two characters and hh != 12, add 12 to hh. IF hh == 12, then do nothing
+    if (meridiem == "PM" && hours != 12 ) {
+        hours += 12;
+    }
 
-//if PM is last two characters and hh != 12, add 12 to hh. IF hh == 12, then do nothing
+    //Convert hours back to string and incorporate these revised hours into militaryTime string, truncating meridiem.
+    string militaryHours = "";
+    militaryHours = to_string (hours);
 
-
+    for (int i = 0; i < 8; i ++) {
+        if (i < 2) {
+            militaryTime [i] = militaryHours [i];
+        }
+        else {
+            militaryTime [i] = s[i];
+        }
+    }
+    return militaryTime;
 }
 
 int main()
